af1528212dc28d773f8e9f66c9b9c5f3b6dd68c7
Joel-Ortiz-Becerra <joelortizbecerra01@gmail.com>
2025-12-19 10:38:23 +0100

    Post y Delete def

--- backend/api/models/userModel.js
+++ backend/api/models/userModel.js
@@ -2,7 +2,13 @@ import pool from '../config/db.js'
 
 export const insertUser = async (userData) => {
 
-    const { nombre, nickname, email, contrasena } = userData
+    const { nombre, nickname, email, contrasena } = userData;
+
+    if (!nombre || !nickname || !email || !contrasena) {
+        return res.status(400).json({
+            message: "Por favor, rellena todos los campos obligatorios."
+        });
+    }
 
     const [result] = await pool.query(
         'INSERT INTO usuarios (nombre, nickname, email, contrasena)VALUES( ?, ?, ?, ?)',
--- backend/api/routes/userRoutes.js
+++ backend/api/routes/userRoutes.js
@@ -4,10 +4,6 @@ import { register, deleteUser  } from '../controllers/userController.js'
 
 const router = express.Router();
 
-// ==============================
-// POST USER
-// ==============================
-
 router.post('/', register);
 
 router.delete('/:id', deleteUser);
