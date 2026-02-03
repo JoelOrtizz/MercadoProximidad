2d2afc6438c7c343a48cfadcf249e1326f25a9d9
tekilaTime <dasge97@gmail.com>
2026-02-03 13:03:21 +0100

    ruto notificaciones añadida

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -28,6 +28,8 @@ const router = createRouter({
     { path: '/mensajes', component: MensajesView, meta: { css: '/css/mensajes.css' } },
     { path: '/mensajes/:id', component: MensajesView, meta: { css: '/css/mensajes.css' } },
     { path: '/valoraciones', component: ValoracionesView, meta: { css: '/css/valoraciones.css' } },
+    { path: '/notificaciones', component: NotificacionesView, meta: { css: '/css/index.css' } },
+    { path: '/usuario/:id', component: PerfilUsuarioView, meta: { css: '/css/index.css' } },
     { path: '/:pathMatch(.*)*', redirect: '/comprar' },
   ],
 });
